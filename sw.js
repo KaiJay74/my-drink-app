be16d686d97c802e33eb2d4b8aa6136c4a3d44a0
KaiJay74 <83072972+KaiJay74@users.noreply.github.com>
2026-01-02 14:34:11 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "my-drink-app-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
